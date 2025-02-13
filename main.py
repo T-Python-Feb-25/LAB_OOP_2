2ca98af9725bd76f947b488a92f1feca79865004
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-13 14:05:18 +0300

    Solution for LAB_OOP_2

---
+++ main.py
@@ -0,0 +1,14 @@
+from bankaccount import BankAccount
+ahmed_account=BankAccount("ahmed")
+try:
+    ahmed_account.withdraw(10000)
+except Exception as e:
+    print(e)
+
+print(ahmed_account.get_balance())
+khaild_account=BankAccount("khaild",1000)
+print(khaild_account.deposit(100))
+print(khaild_account.withdraw(1000))
+print(khaild_account.get_account_holder())
+print(khaild_account.get_balance())
+
