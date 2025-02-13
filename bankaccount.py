4915e428b72fffc471b37c5fd29235430a89b396
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-13 14:19:49 +0300

    Solution for LAB_OOP_2

--- bankaccount.py
+++ bankaccount.py
@@ -14,22 +14,22 @@ Create a Python class called `BankAccount` that simulates a simple bank account.
 class BankAccount:
     def __init__(self,name,initial_balance=0):
         self.__account_holder=name
-        self.__initial_balance=initial_balance
+        self.__balance=initial_balance
     def deposit(self,amount):
         '''`deposit` a function that accepts an amount and adds it to the account balance, and then returns the updated balance.'''
-        self.__initial_balance+=amount
-        return self.__initial_balance
+        self.__balance+=amount
+        return self.__balance
     def withdraw(self,amount):
         '''`withdraw` a function that accepts an amount and subtracts it from the account balance, returning the updated balance, but only if there are sufficient funds in the account. If there are insufficient funds, it should raise an Exception and leave the balance unchanged '''
-        if self.__initial_balance < amount:
-            raise Exception(f"Can't withdraw {amount}, it is greater than current balance of {self.__initial_balance}.")
+        if self.__balance < amount:
+            raise Exception(f"Can't withdraw {amount}, it is greater than current balance of {self.__balance}.")
         else:
-            self.__initial_balance-=amount
-            return self.__initial_balance
+            self.__balance-=amount
+            return self.__balance
         
     def get_balance(self):
         '''`get_balance` a function that returns the current account balance.'''
-        return self.__initial_balance
+        return self.__balance
     def get_account_holder(self):
         ''' `get_account_holder` a function that returns the name of the account holder'''
         return self.__account_holder
