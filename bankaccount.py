05f24547cfccdbd8f9bbcb98180dc5010e7e9eda
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-13 14:15:07 +0300

    Solution for LAB_OOP_2

--- bankaccount.py
+++ bankaccount.py
@@ -4,7 +4,8 @@ Create a Python class called `BankAccount` that simulates a simple bank account.
 
 1. It should have a constructor that accepts the `account_holder` name and initial balance (`initial_balance`), setting the balance to zero if the initial balance is not provided.
 2. A method called `deposit` that accepts an amount and adds it to the account balance, and then returns the updated balance.
-3. A method called `withdraw` that accepts an amount and subtracts it from the account balance, returning the updated balance, but only if there are sufficient funds in the account. If there are insufficient funds, it should raise an Exception and leave the balance unchanged (make sure to handle the exception when calling the method).
+3. A method called `withdraw` that accepts an amount and subtracts it from the account balance, returning the updated balance, but only if there are sufficient funds in the account. If there are insufficient funds, it should raise an Exception and leave the balance unchanged 
+(make sure to handle the exception when calling the method).
 4. A method called `get_balance` that returns the current account balance.
 5. A method called `get_account_holder` that returns the name of the account holder.
 
@@ -15,9 +16,11 @@ class BankAccount:
         self.__account_holder=name
         self.__initial_balance=initial_balance
     def deposit(self,amount):
+        '''`deposit` a function that accepts an amount and adds it to the account balance, and then returns the updated balance.'''
         self.__initial_balance+=amount
         return self.__initial_balance
     def withdraw(self,amount):
+        '''`withdraw` a function that accepts an amount and subtracts it from the account balance, returning the updated balance, but only if there are sufficient funds in the account. If there are insufficient funds, it should raise an Exception and leave the balance unchanged '''
         if self.__initial_balance < amount:
             raise Exception(f"Can't withdraw {amount}, it is greater than current balance of {self.__initial_balance}.")
         else:
@@ -25,8 +28,10 @@ class BankAccount:
             return self.__initial_balance
         
     def get_balance(self):
+        '''`get_balance` a function that returns the current account balance.'''
         return self.__initial_balance
     def get_account_holder(self):
+        ''' `get_account_holder` a function that returns the name of the account holder'''
         return self.__account_holder
 
 
--- main.py
+++ main.py
@@ -8,7 +8,11 @@ except Exception as e:
 print(ahmed_account.get_balance())
 khaild_account=BankAccount("khaild",1000)
 print(khaild_account.deposit(100))
-print(khaild_account.withdraw(1000))
+try:
+    print(khaild_account.withdraw(1000))
+except Exception as e:
+    print(e)
+
 print(khaild_account.get_account_holder())
 print(khaild_account.get_balance())
 
